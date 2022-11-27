56379bbe057497435fa982e3ceca616f9d26cb11
John O'Brien <72422063+J-O-B@users.noreply.github.com>
2022-11-27 15:12:13 +0000

    Update urls.py
    added 404 routing to urls

--- Project/extension/urls.py
+++ Project/extension/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('app.urls')),
 ]
+
+handler404 = "extension.views.page_not_found"
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
