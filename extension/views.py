6322d412b8fd08513c72a6861eb1f21b99777a24
John O'Brien <72422063+J-O-B@users.noreply.github.com>
2022-12-02 20:17:58 +0000

    updated views
    updated view with content length obfuscation

--- extension/views.py
+++ extension/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+import random
+
+def obfuscate():
+    num = random.randint(50, 1000)
+    chars = 'a' * num
+    return chars
 
 def page_not_found(request, exception):
-  return render(request, 'errorpage.html', status=404)
+  template = 'errorpage.html'
+  context = {
+    'obf': obfuscate()
+  }
+  return render(request, template, context, status=200)
